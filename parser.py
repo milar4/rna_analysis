46ee86a45752eb1f3bab3e04b61b0391f5decddf
Familia <esanzgar@bigcat.default>
2016-04-27 23:09:11 +0100

    Version mejorada.

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ Dictionary keys are PMID and values are arrays.
 
 import csv
 import sys
+import re
 from retrieve_rnacentral_id import get_md5, get_rnacentral_id
 
 
@@ -15,15 +16,11 @@ def is_pmid_valid(pmid):
 
     Returns True/False
     """
-    if pmid == '':
+    if re.match("^\d+$", pmid):
+        return True
+    else:
+        print pmid
         return False
-
-    for character in pmid:
-        # if character is in the list of values
-        if character not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
-            return False
-
-    return True
 
 
 def is_sequence_valid(sequence):
@@ -32,21 +29,12 @@ def is_sequence_valid(sequence):
 
     Returns True/False
     """
-    if sequence == '':
-        return False
-
-    # TODO: match regular expression instead of the following lines
-    for nucleotide in sequence:
-        # if character is in the list of values
-        if nucleotide not in ('A', 'T', 'U', 'G', 'C', 'R', 'Y', 'M', 'K',
-                              'W', 'S', 'B', 'D', 'H', 'V', 'N', ' ', '\n'):
-
-            print 'sequence not valid (' + sequence + ')'
-            print '*' + nucleotide + '*'
+    if re.match("^[ATUGCRYMKWSBDHN\s]+$", sequence, flags=re.I):
+        return True
+    else:
+        if sequence != '-':
+            print sequence
             return False
-
-    return True
-
 
 def clean_sequence(sequence):
     """
@@ -54,41 +42,69 @@ def clean_sequence(sequence):
 
     Returns new string
     """
-    new_sequence = sequence.replace('\n', '')
-    new_sequence = new_sequence.replace(' ', '')
-    return new_sequence
-
+    if is_sequence_valid(line[3]):
+        new_sequence = re.sub (r'\s','', sequence)
+        return new_sequence
 
 # opens the file and assignes it to the variable my_file
 # TODO: pass the path and name of the file through an argument.
-with open(sys.argv[1]) as my_csv:
-    my_file = csv.reader(my_csv)
-
+total_number_of_papers = 0
+total_RNA_sequences = 0
+total_RNAcentral_IDs = 0
+for fyle in sys.argv[1:]:
+    with open(fyle) as my_csv:
+        my_file = csv.reader(my_csv)
     # create empty dictionary
-    results = {}
+        results = {}
+        id_central = {}
     # reads the first two lines and discards them
-    my_file.next()
-    my_file.next()
+        my_file.next()
+        my_file.next()
     # reads from third line and assignes it to variable line
-    previous_pmid = ""
-    for line in my_file:
+        previous_pmid = ""
+        total = 0
+        have_rna_central_ID = 0
+        papers = 0
+        for line in my_file:
         # check key is valid, if not use previous key
         # if is calling a function which returns True or False
-        if is_pmid_valid(line[1]):
-            pmid = line[1]
-            previous_pmid = pmid
-        else:
-            pmid = previous_pmid
-
-        if is_sequence_valid(line[3]):
-            sequence = clean_sequence(line[3])
-            md5 = get_md5(sequence)
-            result = get_rnacentral_id(md5)
-            print result
-            if pmid in results:
-                results[pmid].append(sequence)
+            if is_pmid_valid(line[1]):
+                pmid = line[1]
+                previous_pmid = pmid
+                papers = papers + 1
             else:
-                results[pmid] = [sequence]
+                pmid = previous_pmid
+            if is_sequence_valid(line[3]):
+                sequence = clean_sequence(line[3])
+                total = total + 1
+                md5 = get_md5(sequence)
+                result = get_rnacentral_id(md5)
+                print result
+                if pmid in results:
+                    results[pmid].append(sequence)
+                else:
+                    results[pmid] = [sequence]
+                if result !='':
+                    if pmid in id_central:
+                        id_central[pmid].append(result)
+                    else:
+                        id_central[pmid] = [result]
+                    have_rna_central_ID = have_rna_central_ID + 1
+
+    percentage_in_RNAcentral = float(have_rna_central_ID) / total * 100
     print results
+    print id_central
+    print " In %d papers we found %d RNA sequences" %(papers, total)
+    print " %d have RNA central ID" % have_rna_central_ID
+    print " %f pencent have RNA central ID" % percentage_in_RNAcentral
+    total_number_of_papers = total_number_of_papers + papers
+    total_RNA_sequences = total_RNA_sequences + total
+    total_RNAcentral_IDs = total_RNAcentral_IDs + have_rna_central_ID
+    total_percentage_in_RNAcentral = float(total_RNAcentral_IDs) / total_RNA_sequences * 100
+print " TOTAL RESULTS: In %d papers we found %d sequences" %(total_number_of_papers, total_RNA_sequences)
+print " %d have RNA central ID (%f percent)" %(total_RNAcentral_IDs, total_percentage_in_RNAcentral)
+
+
+
 
 # TODO: serialize the dictionary
